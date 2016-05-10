6bb52d1d1e2c52116a3fdd760dcac5ed5d97e9b3
Phil Hargett <phil@haphazardhouse.net>
2016-05-09 18:59:32 -0700

    Cleanup Simple implementation

--- src/Control/Consensus/Paxos/Simple.hs
+++ src/Control/Consensus/Paxos/Simple.hs
@@ -34,22 +34,13 @@ type Members d = M.Map MemberId (Member d)
 
 mkProposer :: (Decreeable d) => Members d -> Proposer d
 mkProposer members = Proposer {
-  prepare = simplePrepare members,
-  propose = simplePropose members,
-  accept = simpleAccept members
+  prepare = mcall members onPrepare,
+  propose = mcall members onPropose,
+  accept = mcall members onAccept
 }
 
-simplePrepare :: (Decreeable d) => Members d -> Member d -> Prepare -> IO (Votes d)
-simplePrepare members member prep = mcall members member prep onPrepare
-
-simplePropose :: (Decreeable d) => Members d -> Member d -> Proposal d-> IO (Votes d)
-simplePropose members member prop = mcall members member prop onPropose
-
-simpleAccept :: (Decreeable d) => Members d -> Member d -> Decree d -> IO (M.Map MemberId (Maybe Bool))
-simpleAccept members member d = mcall members member d onAccept
-
-mcall :: (Serialize a,Serialize r) => Members d -> Member d -> a -> (Member d -> a -> IO r) -> IO (M.Map MemberId (Maybe r))
-mcall members member arg fn = do
+mcall :: (Serialize a,Serialize r) => Members d -> (Member d -> a -> IO r) -> Member d -> a -> IO (M.Map MemberId (Maybe r))
+mcall members fn member arg = do
   results <- mapM call $ S.elems $ paxosMembers member
   return $ M.fromList results
   where
